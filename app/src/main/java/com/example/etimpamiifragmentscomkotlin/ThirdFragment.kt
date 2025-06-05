70a1f02018d733008e644290e1a529f3dcbaeec3
analuizaakin <analuizaakinroyeje@gmail.com>
2025-06-05 20:19:18 -0300

    Atividade de PAM

---
+++ app/src/main/java/com/example/etimpamiifragmentscomkotlin/ThirdFragment.kt
@@ -0,0 +1,45 @@
+package com.example.etimpamiifragmentscomkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class ThirdFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_third, container, false)
+
+        val buttonRosa = view.findViewById<Button>(R.id.buttonrosa)
+        val buttonAmarelo = view.findViewById<Button>(R.id.buttonamarelo)
+        val buttonVerde = view.findViewById<Button>(R.id.buttonverde)
+
+        buttonRosa.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, FirstFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        buttonAmarelo.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, SecondFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        buttonVerde.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, ThirdFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        return view
+    }
+}
